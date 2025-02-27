6298c168241fb820e77616cd8c8f68f74fb58dd6
landroide <landrade1313@gmail.com>
2025-02-27 17:27:32 +1300

    CRSF

--- blog/settings.py
+++ blog/settings.py
@@ -123,3 +123,6 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-741d2.up.railway.app/']
+
